f430011bac123f40e504191e438b21bc0b2d940d
gnfkdskql <70249565+gnfkdskql@users.noreply.github.com>
2021-07-18 03:34:07 +0900

    ver1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Monster from "./Component/Monster.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const monsterList = [
+    {
+        name: "스타로드",
+        attack: 0,
+        magic: 5,
+        debuff: ["실명, 빙결, 화상, 기절"],
+        elemental: ["불속성"],
+        img: "/img/starload.png",
+        etc: "",
+    },
+    {
+        name: "헬",
+        attack: 8,
+        magic: 0,
+        debuff: ["방깍, 중독, 실명, 기절"],
+        elemental: [""],
+        img: "/img/hel.png",
+        etc: "디버프상태면 좆댐",
+    },
+    {
+        name: "수르트",
+        attack: 6,
+        magic: 1,
+        debuff: ["방깍, 공깍, 출혈"],
+        elemental: ["불속성"],
+        img: "/img/surtr.png",
+        etc: "디버프상태면 좆댐",
+    },
+];
+
+class App extends React.Component {
+    state = {
+        monsters: [],
+    };
+
+    componentDidMount() {
+        this.setState({ monsters: monsterList });
+    }
+    render() {
+        const { monsters } = this.state;
+        return (
+            <div className="App">
+                <div className="monsters">
+                    {monsters.map((monster) => (
+                        <Monster
+                            key={monster.name}
+                            name={monster.name}
+                            attack={monster.attack}
+                            magic={monster.magic}
+                            debuff={monster.debuff}
+                            img={monster.img}
+                            etc={monster.etc}
+                            elemental={monster.elemental}
+                        />
+                    ))}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Component/Monster.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Monster.css";
+
+function Monster({
+    name,
+    attack,
+    magic,
+    debuff,
+    elemental,
+    img,
+    etc,
+}) {
+    return (
+        <div>
+            <img src={process.env.PUBLIC_URL + img} alt="" />
+            <h1>{name}</h1>
+            <h2>
+                물리기술: {attack}
+                <br />
+                마법기술: {magic}
+            </h2>
+            <h3>
+                상태이상: {debuff}
+                <br />
+                공격속성: {elemental}
+            </h3>
+        </div>
+    );
+}
+
+export default Monster;
