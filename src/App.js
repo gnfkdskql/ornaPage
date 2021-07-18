efdd049d200bb007ce39da27cdac94161e98a45f
gnfkdskql <70249565+gnfkdskql@users.noreply.github.com>
2021-07-18 22:39:36 +0900

    ver1.1

--- src/App.js
+++ src/App.js
@@ -30,6 +30,42 @@ const monsterList = [
         img: "/img/surtr.png",
         etc: "디버프상태면 좆댐",
     },
+    {
+        name: "페이게이저",
+        attack: 1,
+        magic: 5,
+        debuff: ["실명, 저주, 공깎, 주문깎, 방깍, 저항깎"],
+        elemental: ["아케인, 불, 물, 번개, 땅"],
+        img: "/img/fey_gazer.png",
+        etc: "4속성상태이상걸림!!!",
+    },
+    {
+        name: "페이드래곤",
+        attack: 3,
+        magic: 3,
+        debuff: ["역병, 화상"],
+        elemental: ["불, 용"],
+        img: "/img/fey_dragon.png",
+        etc: "",
+    },
+    {
+        name: "퍼스트호스맨",
+        attack: 2,
+        magic: 5,
+        debuff: ["부폐"],
+        elemental: ["어둠, 땅, 빛"],
+        img: "/img/horseman_famine.png",
+        etc: "한방기있음!!",
+    },
+    {
+        name: "세컨드호스맨",
+        attack: 0,
+        magic: 4,
+        debuff: ["역병, 중독, 맹독, 부폐, 마비, 화상"],
+        elemental: ["빛, 어둠"],
+        img: "/img/horseman_pestilence.png",
+        etc: "한방기있음!!",
+    },
 ];
 
 class App extends React.Component {
